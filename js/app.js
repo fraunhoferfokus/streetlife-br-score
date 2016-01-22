1b44c3ff2ae9e580627b4c118fe0b55e60bf1b8f
Manuel Polzhofer <manuel.polzhofer@fokus.fraunhofer.de>
2016-01-22 17:59:06 +0100

    added latest score

--- js/app.js
+++ js/app.js
@@ -9,25 +9,16 @@ angular.module('scoreApp', [])
         $scope.sortReverse  = false;  // set the default sort order
         $scope.searchUser   = '';     // set the default search/filter term
 
-        // create the list of sushi rolls
-        $scope.sushi = [
-            { name: 'Cali Roll', fish: 'Crab', tastiness: 2 },
-            { name: 'Philly', fish: 'Tuna', tastiness: 4 },
-            { name: 'Tiger', fish: 'Eel', tastiness: 7 },
-            { name: 'Rainbow', fish: 'Variety', tastiness: 6 }
-        ];
-
-
 
 
         // create the list of sushi rolls
         $scope.scoreList = [
-            { username: 'carsten', trees: 11, score: 120,rank:1 },
-            { username: 'manuel', trees: 7, score: 105,rank:2 },
-            { username: 'ben', trees: 5, score: 95,rank:3 },
-            { username: 'bjoern', trees: 4, score: 83,rank:4 },
-            { username: 'silke', trees: 3, score: 56,rank:5},
-            { username: 'florian', trees: 0, score: 7,rank:6 }
+            { username: 'carsten', trees: 11, score: 120,actualSeason:40,rank:1 },
+            { username: 'manuel', trees: 7, score: 105,actualSeason:52,rank:2 },
+            { username: 'ben', trees: 5, score: 95,actualSeason:32,rank:3 },
+            { username: 'bjoern', trees: 4, score: 83,actualSeason:44,rank:4 },
+            { username: 'silke', trees: 3, score: 56,actualSeason:20,rank:5},
+            { username: 'florian', trees: 0, score: 7,actualSeason:7,rank:6 }
 
         ];
 
