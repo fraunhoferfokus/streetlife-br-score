98c8db930bd0925f7372113ea71f2ed4eb611c45
Manuel Polzhofer <manuel.polzhofer@fokus.fraunhofer.de>
2016-02-18 17:02:42 +0100

    comments added

--- js/app.js
+++ js/app.js
@@ -48,13 +48,13 @@ angular.module('scoreApp', ['ngRoute'])
             }).then(function successCallback(response) {
                 // this callback will be called asynchronously
                 // when the response is available
-               // console.log(response);
+                console.log(response);
                 $scope.showMessageSend = true;
                 $scope.showContactForm = false;
             }, function errorCallback(response) {
                 // called asynchronously if an error occurs
                 // or server returns response with an error status.
-               // console.log(response);
+                console.log(response);
                 $scope.showMessageSendError = true;
                 $scope.showContactForm = false;
             });
